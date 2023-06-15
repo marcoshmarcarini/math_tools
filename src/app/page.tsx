6c50e794bafaaf80852a4719d5b08e099d6b694c
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-06-15 17:33:10 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:bg-transparent lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-chumbo dark:via-ferro lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'chumbo': '#2d3436',
+      'ferro': '#34495e'
+    },
   },
   plugins: [],
 }
