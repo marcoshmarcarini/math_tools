9be3e25544ea1b578fb9460edf1f9740a3101c81
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-06-20 17:09:10 -0300

    Update page.ts

--- src/app/pdf-to-image/page.ts
+++ src/app/pdf-to-image/page.ts
@@ -1,40 +1,38 @@
-//import { NextApiRequest, NextApiResponse } from 'next';
 import { fromPath } from 'pdf2pic';
 import { tmpdir } from 'os';
 import { join } from 'path';
+import bodyParser from 'body-parser';
 
-export default async function handler({req, res}:any): Promise<void> {
+const jsonParser = bodyParser.json();
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
+    // Use o middleware body-parser para analisar o corpo da requisição como JSON
     jsonParser(req, res, async () => {
-        const { file } = req.body;
+      const { file } = req.body;
 
-        const options = {
-          density: 100,
-          saveFilename: 'untiled',
-          savePath: join(tmpdir(), 'images'), // Certifique-se de que a pasta "images" existe dentro da pasta de arquivos temporários
-          format: 'png',
-          width: 600,
-          height: 600,
-        };
-    
-        const storeAsImage = fromPath(file, options);
-        const pageToConvertAsImage = 1;
-    
-        try {
-          const result = await storeAsImage(pageToConvertAsImage);
-          console.log('A primeira página foi convertida em uma imagem:', result);
-          res.status(200).json({ success: true, result });
-        } catch (error) {
-          console.error('Erro ao converter PDF para imagem:', error);
-          res.status(500).json({ success: false, error: 'Erro ao converter PDF para imagem' });
-        }
-    })
-    
+      const options = {
+        density: 100,
+        saveFilename: 'untiled',
+        savePath: join(tmpdir(), 'images'),
+        format: 'png',
+        width: 600,
+        height: 600,
+      };
+
+      const storeAsImage = fromPath(file, options);
+      const pageToConvertAsImage = 1;
+
+      try {
+        const result = await storeAsImage(pageToConvertAsImage);
+        console.log('A primeira página foi convertida em uma imagem:', result);
+        res.status(200).json({ success: true, result });
+      } catch (error) {
+        console.error('Erro ao converter PDF para imagem:', error);
+        res.status(500).json({ success: false, error: 'Erro ao converter PDF para imagem' });
+      }
+    });
   } else {
     res.status(405).end('Método não permitido');
   }
-}
-
-function jsonParser(req: any, res: any, arg2: () => Promise<void>) {
-    throw new Error('Function not implemented.');
 }
